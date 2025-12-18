024df3c53a32a7288be0153e96f7d98b8f7c1e70
Ilhamnk77 <ilhamnk787@gmail.com>
2025-12-18 12:23:02 +0700

    pembenaran iframe

--- resources/views/ctemplate/create.blade.php
+++ resources/views/ctemplate/create.blade.php
@@ -1,7 +1,16 @@
 <div class="p-3">
     <h4 class="pt-3 pb-2">Certificate Template</h4>
-    <a href="/ctemplates" class="btn btn-primary mb-4"><i class="fas fa-arrow-left mr-2"></i>Back</a>
-    <iframe src="{{ $url }}"
-        sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-top-navigation"
-        allow="clipboard-read; clipboard-write" width="100%" height="1100" frameborder="0"></iframe>
+
+    <a href="/ctemplates" class="btn btn-primary mb-4">
+        <i class="fas fa-arrow-left mr-2"></i>Back
+    </a>
+
+    <iframe
+        src="{{ $url }}"
+        sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-top-navigation allow-modals"
+        allow="clipboard-read; clipboard-write"
+        width="100%"
+        height="1100"
+        frameborder="0">
+    </iframe>
 </div>
